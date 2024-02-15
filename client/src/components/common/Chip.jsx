14d84660e20eda541c35e111be1d8f73cc570432
anuj <anujsbhuyar@gmail.com>
2024-02-15 11:33:52 +0530

    create empty card

--- client/src/components/common/Chip.jsx
+++ client/src/components/common/Chip.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
-
-const Chip = ({ label }) => <p className='text-xs bg-gradient-to-r from-blue-500 to-blue-300 text-white py-1 px-2 rounded-full capitalize'>{label}</p>;
+const Chip = ({ label }) => (
+  <p className="text-xs bg-gradient-to-r from-blue-500 to-blue-300 text-white py-1 px-2 rounded-full capitalize">
+    {label}
+  </p>
+);
 
 export default Chip;
